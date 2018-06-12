894f3b92d7167c1a2879615a2b70b73a4eb9d70a
Mark Deanil Vicente <markdeanilvicente@outlook.com>
2018-06-12 14:01:53 +0800

    Add project files.

---
+++ ASPNETCoreForWsSetup/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.WindowsServices;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace ASPNETCoreForWsSetup
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var pathToExe = Process.GetCurrentProcess().MainModule.FileName;
+            var pathToContentRoot = Path.GetDirectoryName(pathToExe);
+
+            var host = WebHost.CreateDefaultBuilder(args)
+                .UseContentRoot(pathToContentRoot)
+                .UseStartup<Startup>()
+                .UseUrls("http://*:1222")
+                .Build();
+
+            host.RunAsService();
+        }
+    }
+}
